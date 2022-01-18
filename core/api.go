b8cb0181997d49c62069886987b50b82ec34471e
Georgios Giatrakos <ggiatrakos@programize.com>
2022-01-18 11:10:47 +0200

    Add torrent prompt
    + Add prompt for selecting torrent
    + Fix minor issues with movieSelector
    + Add downloadFile function for downloading
    the user selected torrent file
    + Reformat/ Refactor

--- core/api.go
+++ core/api.go
@@ -1,43 +1,53 @@
 package core
 
 import (
-	"net/http"
-	"fmt"
 	"encoding/json"
-	"log"
+	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
-	// "strconv"
 	"github.com/manifoldco/promptui"
-	// "errors"
-
 )
 
 const baseUrl = "https://yts.mx/api/v2/list_movies.json"
 
-type Movie struct {
-	Id int `json:id`
-	Url string `json:url`
-	ImdbCode string `json:imdb_code`
-	Title string `json:title`
-	Year int `json:year`
-	Summary string `json:summary`
+type Torrent struct {
+	Url      string `json:"url"`
+	Hash     string `json:"hash"`
+	Quality  string `json:"quality"`
+	Type     string `json:"type"`
+	Seeds    int    `json:"seeds"`
+	Peers    int    `json:"peers"`
+	Size     string `json:"size"`
+	Uploaded string `json:"date_uploaded"`
 }
 
+type Movie struct {
+	Id       int       `json:"id"`
+	Url      string    `json:"url"`
+	ImdbCode string    `json:"imdb_code"`
+	Title    string    `json:"title"`
+	Year     int       `json:"year"`
+	Summary  string    `json:"summary"`
+	Slug     string    `json:"slug"`
+	Torrents []Torrent `json:"torrents"`
+}
 
 type MovieData struct {
-	Count int `json:movie_count`
-	Limit int `json:limit`
-	Page int `json:page_number`
-	Movies []Movie `json:movies`
+	Count  int     `json:"movie_count"`
+	Limit  int     `json:"limit"`
+	Page   int     `json:"page_number"`
+	Movies []Movie `json:"movies"`
 }
 
-type YTSResponse struct  {
-	Status string `json:string`
-	Message string `json:status_message`
-	Data MovieData `json:data`
+type YTSResponse struct {
+	Status  string    `json:"string"`
+	Message string    `json:"status_message"`
+	Data    MovieData `json:"data"`
 }
-
 
 // Code from https://mailazy.com/blog/http-request-golang-with-best-practices/ tutorial
 
@@ -68,7 +78,7 @@ func SearchMovie(movie string) {
 		log.Fatal(err)
 	}
 
-	if(resp.Status != "200 OK") {
+	if resp.Status != "200 OK" {
 		log.Print("Error reaching api")
 		return
 	}
@@ -77,28 +87,91 @@ func SearchMovie(movie string) {
 	//fmt.Println(string(responseBody))
 
 	SelectMovie(yts.Data.Movies)
-	
+
 }
 
 func SelectMovie(movies []Movie) {
-	var Items []string
-	for _, movie:= range movies {
-		Items = append(Items, movie.Title)
+
+	movieTemplate := &promptui.SelectTemplates{
+		Label:    "{{ .Title | cyan}} {{ .Year | cyan}}",
+		Active:   "{{ .Title | red }} {{ .Year | red}}",
+		Inactive: "{{ .Title | cyan}} {{ .Year | cyan}}",
+		Selected: "{{ .Title | cyan}} {{ .Year | cyan}}",
 	}
 
-	prompt := promptui.Select{
-		Label: "Select Movie",
-		Items: Items,
-		Size: 20,
+	movieSelector := promptui.Select{
+		Label:     "Select Movie",
+		Items:     movies,
+		Size:      20,
+		Templates: movieTemplate,
 	}
-	
-	pos, result, err := prompt.Run()
+
+	pos, _, err := movieSelector.Run()
 
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 		return
 	}
 
-	fmt.Printf("You choose the %d option %q\n", pos, result)
+	selectedMovie := movies[pos]
+	torrentTemplate := &promptui.SelectTemplates{
+		Label:    "Quality: {{ .Quality | cyan}} {{ .Seeds | green}}/{{ .Peers | red}} (s/p)",
+		Active:   "Quality: {{ .Quality | red}} {{ .Seeds | green}}/{{ .Peers | red}} (s/p)",
+		Inactive: "Quality: {{ .Quality | cyan}} {{ .Seeds | green}}/{{ .Peers | red}} (s/p)",
+		Selected: "Quality: {{ .Quality | cyan}} {{ .Seeds | green}}/{{ .Peers | red}} (s/p)",
+	}
 
+	torrentSelector := promptui.Select{
+		Label:     "Select Torrent",
+		Items:     selectedMovie.Torrents,
+		Size:      20,
+		Templates: torrentTemplate,
+	}
+
+	posTorrent, _, err := torrentSelector.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	selectedTorrent := selectedMovie.Torrents[posTorrent]
+
+	filePath := fmt.Sprintf("./data/%s.torrent", selectedMovie.Slug)
+
+	downloadFile(filePath, selectedTorrent.Url)
+}
+
+/*
+	We download the torrent file in the provided url and store in ./data
+*/
+
+func downloadFile(filepath string, url string) (err error) {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- core/session.go
+++ core/session.go
@@ -2,12 +2,13 @@ package core
 
 import (
 	"fmt"
-	"github.com/cenkalti/rain/torrent"
-	"time"
 	"log"
-	"os/signal"
 	"os"
+	"os/signal"
 	"syscall"
+	"time"
+
+	"github.com/cenkalti/rain/torrent"
 )
 
 func sessionTorrentInfo(ses *torrent.Session) {
@@ -18,25 +19,24 @@ func sessionTorrentInfo(ses *torrent.Session) {
 	}
 }
 
-func Example()  {
-	session, error := torrent.NewSession(torrent.DefaultConfig);
-	if(error != nil) {
-		fmt.Println("Error creating session");
+func Example() {
+	session, error := torrent.NewSession(torrent.DefaultConfig)
+	if error != nil {
+		fmt.Println("Error creating session")
 	}
 
 	fmt.Println("Torrents: ", session.ListTorrents())
 
 	sigs := make(chan os.Signal, 1)
-    signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	
-    go func() {
-        sig := <-sigs
-        fmt.Println()
-        fmt.Println(sig)
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
 		session.Close()
 		os.Exit(1)
-    }()
+	}()
 
 	file, _ := os.Open("./data/killing_chinese_bookie.torrent")
 
@@ -46,8 +46,7 @@ func Example()  {
 	// Watch the progress
 	for range time.Tick(time.Second) {
 		s := tor.Stats()
-		log.Printf("Status: %s, Downloaded: %d, Total %d, Peers: %d", s.Status.String(), s.Bytes.Completed , s.Bytes.Total, s.Peers.Total)
+		log.Printf("Status: %s, Downloaded: %d, Total %d, Peers: %d", s.Status.String(), s.Bytes.Completed, s.Bytes.Total, s.Peers.Total)
 	}
-
 
 }
