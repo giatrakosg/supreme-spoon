86cd0c2383f314191c2ba34714dec2dd5d1a86c2
Georgios Giatrakos <ggiatrakos@programize.com>
2022-01-21 17:13:20 +0200

    Change bytes to megabytes
    + The progressbar no longer displays just bytes, we convert
    to mb to make it easier

--- core/session.go
+++ core/session.go
@@ -2,58 +2,23 @@ package core
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/cenkalti/rain/torrent"
 	"github.com/schollz/progressbar/v3"
 )
 
-func sessionTorrentInfo(ses *torrent.Session) {
-	torrents := ses.ListTorrents()
-	for i, tor := range torrents {
-		stats := tor.Stats()
-		fmt.Printf("%d: Name: %s, Bytes Completed: %d, Bytes Total %d \n", i, stats.Name, stats.Bytes.Completed, stats.Bytes.Total)
-	}
-}
-
-func Example() {
-	session, error := torrent.NewSession(torrent.DefaultConfig)
-	if error != nil {
-		fmt.Println("Error creating session")
-	}
-
-	fmt.Println("Torrents: ", session.ListTorrents())
-
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		session.Close()
-		os.Exit(1)
-	}()
-
-	file, _ := os.Open("./data/killing_chinese_bookie.torrent")
-
-	sessionTorrentInfo(session)
-
-	tor, _ := session.AddTorrent(file, nil)
-	// Watch the progress
-	for range time.Tick(time.Second) {
-		s := tor.Stats()
-		log.Printf("Status: %s, Downloaded: %d, Total %d, Peers: %d", s.Status.String(), s.Bytes.Completed, s.Bytes.Total, s.Peers.Total)
-	}
-}
+const MEGABYTE = 1e-6
 
 func DownloadTorrent(path string) {
 	torrent.DisableLogging()
-	session, error := torrent.NewSession(torrent.DefaultConfig)
+
+	cfg := torrent.DefaultConfig
+
+	cfg.DataDirIncludesTorrentID = false
+	// cfg.DataDirIncludesTorrentID = false
+	session, error := torrent.NewSession(cfg)
 	if error != nil {
 		fmt.Println("Error creating session")
 	}
@@ -62,15 +27,13 @@ func DownloadTorrent(path string) {
 	tor, _ := session.AddTorrent(file, nil)
 
 	s := tor.Stats()
-	bar := progressbar.Default(s.Bytes.Total)
+
+	totalMb := float64(s.Bytes.Total) * MEGABYTE
+	bar := progressbar.Default(int64(totalMb))
 
 	// Watch the progress
-	previousTotal := 0
 	for range time.Tick(time.Second) {
 		s := tor.Stats()
-		newTotal := s.Bytes.Completed
-		change := int(newTotal) - previousTotal
-		previousTotal = int(newTotal)
-		bar.Add(change)
+		bar.Set64(int64(float64(s.Bytes.Completed) * MEGABYTE))
 	}
 }
