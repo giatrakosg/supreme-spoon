4d313a69e46e922ea9034fc1297335cabeceeb03
Georgios Giatrakos <ggiatrakos@programize.com>
2022-03-06 17:41:24 +0200

    Change app description
    + Change default cobra app description

--- cmd/root.go
+++ cmd/root.go
@@ -17,16 +17,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "supreme-spoon",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Application for searching and downloading movies",
+	Long: `Supreme spoon is an application designed for movie fans. It can be used to search, download and manage
+	movies. For example:
+		supreme-spoon search "The Killing of ".`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
